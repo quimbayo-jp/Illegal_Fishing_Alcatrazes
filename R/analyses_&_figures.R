1f746a9e73465d735a23e89d7d559d1af147a395
quimbayo-jp <quimbayo_jp@usp.br>
2022-03-28 10:11:35 -0400

    Including trait data per species

--- R/analyses_&_figures.R
+++ R/analyses_&_figures.R
@@ -1,1 +1,16 @@
-# 
+## --------------------------------------------------------------------------------------------------------- ##
+##  Authors:       Juan P. Quimbayo, with contributions in the code from:                                    ##
+##                 Fernanda C. Silva, Jonathan S. Lefcheck, Augusto Flores                                   ##
+##  Date:          2021-12-12                                                                                ##
+##                                                                                                           ##
+##  Notes:         1. This file is intended to provide a guide to the basic                                  ##
+##                    project workflow, attempting to 'integrate_analysis' the steps                         ##
+##                    necessary to conduct the analyses & figures.                                           ##
+## --------------------------------------------------------------------------------------------------------- ##
+
+# clean up
+rm(list=ls())
+
+# Calling data
+data   <- read.csv("data/data.csv", header = TRUE, sep = ";", dec=",")
+traits <- 
